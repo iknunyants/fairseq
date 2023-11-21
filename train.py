52a69710a49bd841496fa35a4bc92b252815eb11
Ivan Knunyants <iknunyants@gmail.com>
2023-11-21 17:29:37 +0100

    test repair

--- train.py
+++ train.py
@@ -7,7 +7,7 @@
 Legacy entry point. Use fairseq_cli/train.py or fairseq-train instead.
 """
 
-from fairseq_cli.train import cli_mai
+from fairseq_cli.train import cli_main
 
 
 if __name__ == "__main__":
